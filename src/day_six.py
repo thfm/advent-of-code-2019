2fa30f87b47251d844cf0160f28c911de981f4cf
Thomas Freeman <tomclaudefreeman@gmail.com>
2019-12-16 17:37:29 +1100

    Add day six part one solution

---
+++ src/day_six.py
@@ -0,0 +1,20 @@
+def read_input_orbits(filename):
+    direct_orbits = {}
+    for line in open(filename).readlines():
+        parent, child = line.strip().split(")")
+        direct_orbits[child] = parent
+    return direct_orbits
+
+
+def calculate_orbit_count(orbits):
+    count = 0
+    for orbit in orbits:
+        parent = orbits.get(orbit)
+        while parent != None:
+            count += 1
+            parent = orbits.get(parent)
+    return count
+
+
+INPUT_ORBITS = read_input_orbits("res/day_six_inputs.txt")
+print(calculate_orbit_count(INPUT_ORBITS))
