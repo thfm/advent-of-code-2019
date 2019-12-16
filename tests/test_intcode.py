d26fa905a5ee8b9251c79d42dd8620e2a711a5dd
Thomas Freeman <tomclaudefreeman@gmail.com>
2019-12-16 17:53:00 +1100

    Refactor intcode functionality into seperate module; update source and tests

--- tests/test_intcode.py
+++ tests/test_intcode.py
@@ -1,12 +1,12 @@
 import unittest
-from src import day_five
+from src.intcode import intcode
 
 
 class TestIntcodeComputer(unittest.TestCase):
     def test_instruction_decoding(self):
-        self.assertEqual(day_five.decode_instruction("5"),
+        self.assertEqual(intcode.decode_instruction("5"),
                          ("05", ["0", "0", "0"]))
-        self.assertEqual(day_five.decode_instruction("1104"),
+        self.assertEqual(intcode.decode_instruction("1104"),
                          ("04", ["1", "1", "0"]))
-        self.assertEqual(day_five.decode_instruction("10001"),
+        self.assertEqual(intcode.decode_instruction("10001"),
                          ("01", ["0", "0", "1"]))
