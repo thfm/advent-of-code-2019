5ace3ff55b56d19ac88979b1d345ff0ea7f4fbc6
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-01-27 13:07:43 +1100

    Add comments

--- src/intcode/intcode.py
+++ src/intcode/intcode.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 
 MAX_PARAMS = 3
 
+# The number of parameters used by each instruction
 PARAM_COUNTS = {
     "01": 2,
     "02": 2,
@@ -16,14 +17,16 @@ PARAM_COUNTS = {
 
 def decode_instruction(instruct_code):
     instruct_code = str(instruct_code)
+    # Fills the instruction code up to a certain standardised length
     while len(instruct_code) < MAX_PARAMS + 2:
         instruct_code = "0" + instruct_code
     opcode = instruct_code[-2:]
     param_modes = list(instruct_code[:3])
-    param_modes.reverse()
+    param_modes.reverse() # Param modes are taken from right to left
     return (opcode, param_modes)
 
 
+# See day_two.py 'run_intcode_program' for further details
 def run_intcode_program(program):
     memory = deepcopy(program)
     pointer = 0
@@ -46,7 +49,6 @@ def run_intcode_program(program):
         pointer += param_count
 
         result_addr = memory[pointer]
-
         result = None
         if opcode == "01":
             result = sum(values)
@@ -70,6 +72,7 @@ def run_intcode_program(program):
         elif opcode == "08":
             result = 1 if values[0] == values[1] else 0
 
+        # Not all instructions return an output value
         if result is not None:
             memory[result_addr] = result
             pointer += 1
